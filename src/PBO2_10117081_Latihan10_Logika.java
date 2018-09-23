fe0be2871186780c530810054ad351e3495aa7a7
GeoSyah <43498342+GeoSyah@users.noreply.github.com>
2018-09-23 14:14:10 +0700

    Add files via upload

---
+++ src/PBO2_10117081_Latihan10_Logika.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Geo Syah Alkautsar
+ * 10117081 IF-2
+ * Menampilkan hasil kerja logika di Java
+ */
+public class PBO2_10117081_Latihan10_Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      int umur = 19;
+      boolean dewasa;
+      dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+        
+    }
+    
+}
